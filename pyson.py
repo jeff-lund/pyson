5d1e7e486859295e753d3a4e716fec98b1bb6b60
jeff-lund <jdl3@pdx.edu>
2020-01-07 12:41:34 -0800

    altered parsing to use slices

--- pyson.py
+++ pyson.py
@@ -5,101 +5,120 @@ WHITESPACE = " \n\t\v"
 SYNTAX = '[]{},:'
 QUOTES = '"\''
 NUMERICS = '-.0123456789'
-BOOLS = 'nft'
+FLOAT_CHARACTERS = '.eE'
+BOOLS = 'ft'
+NULLS = 'n'
+ESCAPES = 'bfnrtu\\/\'"'
+HEX_DIGITS = '0123456789abcdefABCDEF'
+HEX_COUNT = 4
+TRUE_LEN = 4
+FALSE_LEN = 5
+NULL_LEN = 4
 
+def lex_string(s):
+    initial = s[0]
+    escaped = False
+    hexed = 0
+    for i in range(1, len(s)):
+        if escaped:
+            if s[i] in ESCAPES:
+                escaped = False
+                if s[i] == 'u':
+                    hexed = HEX_COUNT
+            else:
+                break
+        elif hexed:
+            if s[i] not in HEX_DIGITS:
+                break
+            else:
+                hexed -= 1
+        elif s[i] == initial:
+            return s[1:i], s[i+1:]
+        elif s[i] == '\\':
+            escaped = True
 
-def lex_string(i, s):
-    # TODO add escape characters
-    initial = s[i]
-    i += 1
-    while i < len(s):
-        if s[i] == initial and s[i - 1] != '\\':
-                return i
-        i += 1
-    return None
+    return None, s
 
-def lex_number(i, s):
+def lex_number(s):
+    # Keep working on this, need to parse floats correctly
+    '''
     period = False
-    exp = False
-    
+    exponent = False
+    i = 0
+    # can lead with a negative sign
     if s[i] == '-':
         i += 1
-        if i < len(s) and not s[i].isdigit():
-            return None
-    
+    # can only lead with 1 zero
     if s[i] == '0':
-        if i + 1 < len(s) and s[i + 1] not in 'eE.':
-            return i
+        i += 1
+        if i < len(s):
+            if s[i] == '.':
+                period = True
+            elif s[i] in 'eE':
+                exponent = True
+        else:
+            return 0, s[1:]
+    '''
+    i = 0
+    while i < len(s):
+        if s[i] not in NUMERICS:
+            break
         i += 1
 
-    while i < len(s):
-        if s[i].isdigit():
-            i += 1
-        elif s[i] == '.' and not period:
-            period = True
-            i += 1
-        elif (s[i] == 'e' or s[i] == 'E') and not exp:
-            exp = True
-            i += 1
-            if s[i] in '-+':
-                i += 1
-            if not s[i].isdigit():
-                # and exponent must be followed by a digit
-                # raise a parsing exception here
-                return None
-        else:
-            break
-    return i - 1
+    #if period or exponent:
+    if '.' in s[:i] or 'e' in s[:i] or 'E' in s[:i]:
+        return float(s[:i]), s[i:]
+    else:
+        return int(s[:i]), s[i:]
 
-def lex_bool(i, s):
-    diff = len(s) - i
-    if diff >= 4:
-        if s[i:i+4] == 'true':
-            return i + 3
-        elif s[i:i+4] == 'null':
-            return i + 3
-    if diff >= 5:
-        if s[i:i+5] == 'false':
-            return i + 4
-    return None
+def lex_bool(s):
+    if len(s) >= TRUE_LEN and s[:TRUE_LEN] == 'true':
+        return True, s[TRUE_LEN:]
+    if len(s) >= FALSE_LEN and s[:FALSE_LEN] == 'false':
+        return False, s[FALSE_LEN:]
+    return None, s
+
+def lex_null(s):
+        if len(s) >= NULL_LEN and s[:NULL_LEN] == 'null':
+            return True, s[NULL_LEN:]
+        return None, s
 
 def lex(s):
     tokens = []
     i = 0
-    while i < len(s):
-        if s[i] in WHITESPACE:
-            i += 1
-            continue
-        
-        if s[i] in QUOTES:
-            j = lex_string(i, s)
-            if j is not None:
-                tokens.append(s[i:j+1])
-                i = j + 1
-                continue
-        
-        if s[i].isdigit() or s[i] in NUMERICS:
-            j = lex_number(i, s)
-            if j is not None:
-                t = s[i:j+1]
-                if '.' in t or 'e' in t or 'E' in t:
-                    t = float(t)
-                else:
-                    t = int(t)
-                tokens.append(t)
-                i = j + 1
-                continue
-        
-        if s[i] in SYNTAX:
-            tokens.append(s[i])
-            i += 1
+    while s:
+        char = s[0]
+        if char in WHITESPACE:
+            s = s[1:]
             continue
 
-        if s[i] in BOOLS:
-            j = lex_bool(i, s)
-            if j is not None:
-                tokens.append(s[i:j+1])
-                i = j + 1
+        if char in QUOTES:
+            t, s = lex_string(s)
+            if t is not None:
+                tokens.append(t)
+                continue
+
+        if char.isdigit() or char in NUMERICS:
+            t, s = lex_number(s)
+            if t is not None:
+                tokens.append(t)
+                continue
+
+        if char in SYNTAX:
+            tokens.append(char)
+            s = s[1:]
+            continue
+
+        if char in BOOLS:
+            t, s = lex_bool(s)
+            if t is not None:
+                tokens.append(t)
+                continue
+
+        if char in NULLS:
+            t, s = lex_null(s)
+            if t is not None:
+                tokens.append(None)
                 continue
         # no matching tokens found
         raise Exception("Unexpected character at position {}: {}".format(i, s[i]))
@@ -116,7 +135,7 @@ def parse_obj(tokens):
     t = tokens[0]
     if t == '}':
         return obj, tokens[1:]
-    
+
     while True:
         # get key
         key = t
@@ -124,22 +143,22 @@ def parse_obj(tokens):
             raise Exception("Invalid key value : {}".format(t))
         tokens = tokens[1:]
         validate_tokens(tokens)
-        
+
         # validate delimiter
         t = tokens[0]
         if t != ':':
             raise Exception("Invalid object syntax, missing ':' delimiter between key : value in object. Found: {}".format(t))
         tokens = tokens[1:]
         validate_tokens(tokens)
-        
+
         # get value
         t = tokens[0]
         val, tokens = parse(tokens)
-        
+
         # insert k-v pair into object
         obj[key] = val
         validate_tokens(tokens)
-        
+
         # check for comma for next pair or brace to end object
         t = tokens[0]
         if t == '}':
@@ -156,11 +175,11 @@ def parse_array(tokens):
     arr = []
     if not tokens:
         raise Exception("Left bracket found without matching right bracket: {}".format(tokens))
-    
+
     t = tokens[0]
     if t == ']':
         return arr, tokens[1:]
-    
+
     while True:
         val, tokens = parse(tokens)
         arr.append(val)
@@ -189,8 +208,10 @@ def parse(tokens):
 def pyson(s):
     tokens = lex(s)
     obj, leftover = parse(tokens)
-    return obj, leftover
+    if leftover:
+        raise Exception("Excess data after parsing")
+    return obj
 
 if __name__ == '__main__':
     s = argv[1]
-    print(parse(s))
+    print(pyson(s))
